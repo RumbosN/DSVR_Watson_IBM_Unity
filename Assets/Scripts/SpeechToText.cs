200f96f7d6b67a83b19bc868993bb769f9df9d5c
Pepeelipe <Pepeelipe@gmail.com>
2021-01-07 19:31:29 +0100

    Speech To Text done

---
+++ Assets/Scripts/SpeechToText.cs
@@ -0,0 +1,234 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using IBM.Watson.SpeechToText.V1;
+using IBM.Cloud.SDK;
+using IBM.Cloud.SDK.Authentication.Iam;
+using IBM.Cloud.SDK.Utilities;
+using IBM.Cloud.SDK.DataTypes;
+
+public class SpeechToText : MonoBehaviour
+{
+    public Text OutputText;
+
+    public List<string> listText; 
+    public List<string> listWords;
+
+    [Header("Service URL (optional)")]
+    [SerializeField]
+    private string _serviceUrl;
+
+    [Header("IAM Authentication")]
+    [SerializeField]
+    private string _iamApikey;
+
+    [Header("Model")]
+    [SerializeField]
+    private string _recognizeModel;
+
+    private SpeechToTextService _service;
+
+    private int _recordingRoutine = 0;
+    private string _microphoneID = null;
+    private AudioClip _recording = null;
+    private int _recordingBufferSize = 1;
+    private int _recordingHZ = 22050;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        listText = new List<string>();
+        listWords = new List<string>();
+
+        LogSystem.InstallDefaultReactors();
+        Runnable.Run(CreateService());
+    }
+
+    private IEnumerator CreateService()
+    {
+        if (string.IsNullOrEmpty(_iamApikey))
+        {
+            throw new IBMException("Plesae provide IAM ApiKey for the service.");
+        }
+
+        IamAuthenticator authenticator = new IamAuthenticator(apikey: _iamApikey);
+
+        //  Wait for tokendata
+        while (!authenticator.CanAuthenticate())
+            yield return null;
+
+        _service = new SpeechToTextService(authenticator);
+        if (!string.IsNullOrEmpty(_serviceUrl))
+        {
+            _service.SetServiceUrl(_serviceUrl);
+        }
+        _service.StreamMultipart = true;
+
+        Active = true;
+        StartRecording();
+    }
+
+    public bool Active
+    {
+        get { return _service.IsListening; }
+        set
+        {
+            if (value && !_service.IsListening)
+            {
+                _service.RecognizeModel = (string.IsNullOrEmpty(_recognizeModel) ? "en-US_BroadbandModel" : _recognizeModel);
+                _service.DetectSilence = true;
+                _service.EnableWordConfidence = true;
+                _service.EnableTimestamps = true;
+                _service.SilenceThreshold = 0.01f;
+                _service.MaxAlternatives = 1;
+                _service.EnableInterimResults = true;
+                _service.OnError = OnError;
+                _service.InactivityTimeout = -1;
+                _service.ProfanityFilter = false;
+                _service.SmartFormatting = true;
+                _service.SpeakerLabels = false;
+                _service.WordAlternativesThreshold = null;
+                _service.EndOfPhraseSilenceTime = null;
+                _service.StartListening(OnRecognize, null);
+            }
+            else if (!value && _service.IsListening)
+            {
+                _service.StopListening();
+            }
+        }
+    }
+
+    private void OnError(string error)
+    {
+        Active = false;
+
+        Log.Debug("ExampleStreaming.OnError()", "Error! {0}", error);
+    }
+
+    private void OnRecognize(SpeechRecognitionEvent result)
+    {
+        if (result != null && result.results.Length > 0)
+        {
+            foreach (var res in result.results)
+            {
+                foreach (var alt in res.alternatives)
+                {
+                    //Transcript the audio and ceck if its the final text
+                    string text = string.Format("{0}", alt.transcript);
+
+                    if(res.final)
+                    {
+                        OutputText.text = text;
+
+                        //Add the whole text to the list
+                        listText.Add(text);
+
+                        //Add the words to the list
+                        string[] words = text.Split(' ');
+
+                        foreach (string word in words)
+                        {
+                            if (word != "\n")
+                            {
+                                listWords.Add(word);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private void StartRecording()
+    {
+        if (_recordingRoutine == 0)
+        {
+            UnityObjectUtil.StartDestroyQueue();
+            _recordingRoutine = Runnable.Run(RecordingHandler());
+        }
+    }
+
+    private void StopRecording()
+    {
+        if (_recordingRoutine != 0)
+        {
+            Microphone.End(_microphoneID);
+            Runnable.Stop(_recordingRoutine);
+            _recordingRoutine = 0;
+        }
+    }
+
+    private IEnumerator RecordingHandler()
+    {
+        Log.Debug("ExampleStreaming.RecordingHandler()", "devices: {0}", Microphone.devices);
+        _recording = Microphone.Start(_microphoneID, true, _recordingBufferSize, _recordingHZ);
+        yield return null;      // let _recordingRoutine get set..
+
+        if (_recording == null)
+        {
+            StopRecording();
+            yield break;
+        }
+
+        bool bFirstBlock = true;
+        int midPoint = _recording.samples / 2;
+        float[] samples = null;
+
+        while (_recordingRoutine != 0 && _recording != null)
+        {
+            int writePos = Microphone.GetPosition(_microphoneID);
+            if (writePos > _recording.samples || !Microphone.IsRecording(_microphoneID))
+            {
+                Log.Error("ExampleStreaming.RecordingHandler()", "Microphone disconnected.");
+
+                StopRecording();
+                yield break;
+            }
+
+            if ((bFirstBlock && writePos >= midPoint)
+              || (!bFirstBlock && writePos < midPoint))
+            {
+                // front block is recorded, make a RecordClip and pass it onto our callback.
+                samples = new float[midPoint];
+                _recording.GetData(samples, bFirstBlock ? 0 : midPoint);
+
+                AudioData record = new AudioData();
+                record.MaxLevel = Mathf.Max(Mathf.Abs(Mathf.Min(samples)), Mathf.Max(samples));
+                record.Clip = AudioClip.Create("Recording", midPoint, _recording.channels, _recordingHZ, false);
+                record.Clip.SetData(samples, 0);
+
+                _service.OnListen(record);
+
+                bFirstBlock = !bFirstBlock;
+            }
+            else
+            {
+                // calculate the number of samples remaining until we ready for a block of audio, 
+                // and wait that amount of time it will take to record.
+                int remaining = bFirstBlock ? (midPoint - writePos) : (_recording.samples - writePos);
+                float timeRemaining = (float)remaining / (float)_recordingHZ;
+
+                yield return new WaitForSeconds(timeRemaining);
+            }
+        }
+        yield break;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            foreach(string text in listText)
+            {
+                Debug.Log(text);
+            }
+
+            foreach (string word in listWords)
+            {
+                Debug.Log(word);
+            }
+        }
+    }
+}
